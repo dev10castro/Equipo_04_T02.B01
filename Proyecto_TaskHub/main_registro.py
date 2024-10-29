c92bdb209be52bbe7f1f2a9ed7c7f17b9b0f0ee1
pgonmar505 <pgonmar505@iescarrillo.es>
2024-10-29 18:08:32 +0100

    Revert "Tarea 2 terminada"
    This reverts commit 9d99c67a8860dc09952c9367854305e20dcb6571.

---
+++ Proyecto_TaskHub/main_registro.py
@@ -0,0 +1,17 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from view.Registro_Equipo04_uiV2 import Ui_MainWindow  # Importa la clase Ui_MainWindow generada
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        # Crear una instancia de la UI
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)  # Configura la UI en la ventana principal
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)  # Crea la aplicación
+    window = MainWindow()  # Crea una instancia de la ventana
+    window.show()  # Muestra la ventana
+    sys.exit(app.exec())  # Ejecuta el bucle de eventos
